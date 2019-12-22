637b252db8b2d176637cbb403073697b3f153a46
Nikki <nikkimoteva@gmail.com>
2019-12-21 17:55:11 -0800

    updated Intersection.h

--- Intersection.h
+++ Intersection.h
@@ -1,6 +1,9 @@
-typedef struct Street;
+struct Street;
+enum Direction {NORTH = 0, SOUTH = 1, EAST = 2, WEST = 3};
+const static enum Direction oppositeEnd [] = {SOUTH, NORTH, WEST, EAST};
 void initializeStreet (void);
+void destroyStreet (void);
 void enterStreet (enum Direction d);
 void leaveStreet (void);
-void recordWaitingTime (int waitingTime);
+void waitingTime (int waitingTime);
 void* cars ();
